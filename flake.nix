c1820b930a476330438a66de17aeb69d81a4cb52
Khue Doan <mail@khuedoan.com>
2026-01-21 18:23:25 +0700

    build: add dependencies

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+  in
+  {
+    devShells = supportedSystems (system: {
+      default = with nixpkgs.legacyPackages.${system}; mkShell {
+        packages = [
+          cargo
+          clippy
+          rustc
+          rustfmt
+        ];
+      };
+    });
+  };
+}
